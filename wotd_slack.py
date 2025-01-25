e73c7cbe249d821c051d1942cdb6dc42ffc9fcb6
Barret Obrock <barret@tarkmail.com>
2025-01-25 08:11:24 -0600

    Support getting pronunciations

--- wotd_slack.py
+++ wotd_slack.py
@@ -14,23 +14,48 @@ import requests
 from slack_sdk import WebClient
 
 
-def collect_wotd() -> Dict:
-    url = os.environ['WORD_SOURCE_URL']
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
-        'Cookie': os.environ['COOKIE']
-    }
-    resp = requests.get(url, headers=headers)
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
+}
+
+
+def get_dom(url: str) -> etree.Element:
+    resp = requests.get(url, headers=HEADERS)
     resp.raise_for_status()
+
     soup = BeautifulSoup(resp.content, 'html.parser')
-    dom = etree.HTML(str(soup))
+    return etree.HTML(str(soup))
+
+
+def collect_wotd() -> Dict:
+    dom = get_dom(os.environ['WORD_SOURCE_URL'])
+
     wotd_elem = dom.xpath('.//div[@id="wotd"]/div[@class="content_column"]')[0]
 
+    word = wotd_elem.xpath('./h1/a')[0].text
     pos, definition = [x for x in wotd_elem.xpath('.//div[@id="define"]/div/ul/li')[0].itertext()]
 
+    # Try to get pronunciation
+    pdom = get_dom(f'https://www.dictionary.com/browse/{word}')
+
+    pron = []
+    pron_elem = pdom.xpath('.//div[@data-type="pronunciation-toggle"]/p')[0]
+    for pe in pron_elem.itertext():
+        text_strip = pe.strip()
+        if text_strip == '':
+            continue
+        pron.append(text_strip)
+    for pe in pron_elem.iter():
+        text_strip = pe.text.strip()
+        if text_strip == '':
+            continue
+        if pe.tag == 'strong':
+            if text_strip in pron:
+                pron[pron.index(text_strip)] = f'*{text_strip.upper()}*'
+
     return {
-        'word': wotd_elem.xpath('./h1/a')[0].text,
-        'pronunciation': '¯\_(ツ)_/¯',
+        'word': word,
+        'pronunciation': ''.join(pron),
         'def': definition,
         'part_of_speech': pos,
         'origin': wotd_elem.xpath('.//p[@class="note"]')[0].text
@@ -95,5 +120,5 @@ if __name__ == '__main__':
 
     wotd = collect_wotd()
     blocks = build_blocks(wotd_dict=wotd)
-    res = send_blocks_to_slack(blocks=blocks)
-    logger.debug(f'HTTP response code from Slack: {res.status_code}')
+    # res = send_blocks_to_slack(blocks=blocks)
+    # logger.debug(f'HTTP response code from Slack: {res.status_code}')
